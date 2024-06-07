c0237dc9ceda4173bf69d1580b3b6b850623f828
Aamir-Yousaf <aamir.yousaf.dev@gmail.com>
2024-06-07 20:46:41 +0500

    add contact form

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const main = document.querySelector("#main");
-const cursor = document.querySelector(".cursor");
+// const main = document.querySelector("#main");
+// const cursor = document.querySelector(".cursor");
 
-main.addEventListener("mousemove",(e=>{
-  gsap.to(cursor,{
-    x:e.clientX,
-    y:e.clientY,
-    duration:1
-    // ease:"power2.out"
-  })
-})
+// main.addEventListener("mousemove",(e=>{
+//   gsap.to(cursor,{
+//     x:e.clientX,
+//     y:e.clientY,
+//     duration:1
+//     // ease:"power2.out"
+//   })
+// })
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
